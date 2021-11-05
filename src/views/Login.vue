a0ca537bf20542d13fdb45a89547aba55f5f5750
Haowei_Chang <howard3317@gmail.com>
2021-11-05 10:11:39 +0800

    上傳Login、主頁Layout

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <LoginForm />
+  </div>
+</template>
+<script>
+import LoginForm from "../components/LoginForm.vue";
+export default {
+  name: "Login",
+  components: {
+    LoginForm,
+  },
+};
+</script>
+<style>
+.container {
+  max-width: 500px;
+  margin: 0px auto;
+  overflow: auto;
+  justify-content: center;
+  min-height: 300px;
+  padding: 30px;
+}
+</style>
