77912b1543e7daac23a75f1b819933d9f2da7007
Haowei_Chang <howard3317@gmail.com>
2021-12-27 19:11:56 +0800

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,35 @@
+import axios from "axios";
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+const user = {
+  state: {
+    isLogin: false,
+    caches: {},
+  },
+  mutations: {
+    updateState(state, caches) {
+      const { id, name, isLogin } = caches;
+      if (id && name && isLogin) {
+        state.isLogin = isLogin;
+        state.caches.id = id;
+        state.caches.name = name;
+        localStorage.setItem("user", JSON.stringify(state.caches));
+      } else {
+        state.isLogin = false;
+        state.caches = {};
+        localStorage.clear();
+      }
+    },
+  },
+  getters: {},
+  actions: {
+    async Login({ commit }, payload) {
+      await axios
+        .post("http://localhost:3000/api/auth/Login", payload)
+        .then((res) => commit("updateState", res.data))
+        .catch((error) => console.log(error));
+    },
+  },
+};
+
+export default createStore({ modules: { user } });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,71 @@
 <template>
   <div class="container">
-    <LoginForm />
+    <a-form
+      layout="horizontal"
+      :model="formState"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+      @submit="Login"
+    >
+      <a-form-item ref="user" label="帳號" name="user">
+        <a-input v-model:value="formState.email" placeholder="Account">
+          <template #prefix
+            ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
+          /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item ref="password" label="密碼" name="password">
+        <a-input
+          v-model:value="formState.password"
+          type="password"
+          placeholder="Password"
+        >
+          <template #prefix
+            ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
+          /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button
+          type="primary"
+          html-type="submit"
+          :disabled="formState.user === '' || formState.password === ''"
+        >
+          Log in
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 <script>
-import LoginForm from "../components/LoginForm.vue";
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+import { reactive } from "vue";
+import axios from "axios";
 export default {
   name: "Login",
+  setup() {
+    const formState = reactive({
+      email: "",
+      password: "",
+    });
+    const Login = async () => {
+      await axios.post("http://localhost:3000/api/auth/login", formState);
+    };
+    return {
+      formState,
+      Login,
+      labelCol: {
+        span: 4,
+      },
+      wrapperCol: {
+        span: 14,
+      },
+    };
+  },
+
   components: {
-    LoginForm,
+    UserOutlined,
+    LockOutlined,
   },
 };
 </script>
